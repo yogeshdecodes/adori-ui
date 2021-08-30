01562d498fb76a3447e862e034f9b0ea899de690
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-08-30 11:40:52 +0530

    style: convert grid into flexbox

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 
-const Layout = ({ title, children }) => {
+const Layout = ({ children }) => {
   return (
     <section className="section-layout">
       <Header />
